713b4e9b9778d1935aba60c7de3387d8959d0d8e
Phil Pirj <pirj@mail.ru>
2008-12-28 03:09:28 +0300

    hack to skip {} in the beginning of each template

--- src/retem/retemplate.erl
+++ src/retem/retemplate.erl
@@ -1,8 +1,9 @@
 -module(retemplate).
 -export([parse/1, scan/1]).
 
+% Yes, it's a bad trick, we should get rid of that, but currently there's no way to scan with text in the beginning, thus prepending with braces
 scan({string,String}) ->
-  {ok, Scanned, _EndLine} = retem_scan:string(binary_to_list(String)),
+  {ok, Scanned, _EndLine} = retem_scan:string([${, $} | binary_to_list(String)]),
   Scanned.
 
 parse({string, _String} = Input) ->
