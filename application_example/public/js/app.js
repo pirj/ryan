0a609215b991b2f6179c7393d6717fedc6647406
Phil Pirj <pirj@mail.ru>
2008-12-30 21:54:47 +0300

    neat example app design

--- application_example/public/js/app.js
+++ application_example/public/js/app.js
@@ -1,17 +1,10 @@
 $(document).ready(function() {
 
 	$('div#apples a').livequery('click',function(){
-		var loaded = false
-		$('#apple').fadeOut(.5, function(){
-			if(!loaded)
-				$(this).html('please wait, loading...').fadeIn()
-		})
-
 		$.ajax({
 			url: $(this).attr('href'),
 			dataType: 'html',
 			complete: function(res, status){
-				loaded = true
 				if (status == 'success' || status == 'notmodified'){
 					$('#apple').fadeOut(.5, function(){
 						$(this).html(res.responseText).fadeIn()
