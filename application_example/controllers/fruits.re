1b5d6e1cd7e2d32b4afaa75410ec17764f9ea9f3
Phil Pirj <pirj@mail.ru>
2009-01-07 03:50:10 +0300

    some more n'eyes-candies

--- application_example/controllers/fruits.re
+++ application_example/controllers/fruits.re
@@ -1,15 +1,18 @@
 class Fruits
   def initialize
-    @apples = [{~color: 'red', ~weight: 2.3, ~id: 0}, {~color: 'yellow', ~weight: 1.3, ~id: 1}]
+    @apples = [{~color: 'red', ~weight: 2.3, ~id: 0}, {~color: 'yellow', ~weight: 1.3, ~id: 1}, {~color: 'green', ~weight: 0.6, ~id: 2}]
 
   def index(parameters, cookies)
     bindings = {}.insert(~apples, @apples)
     ('fruits/index', bindings)
 
   def show(parameters, cookies)
-    id = parameters[~id].to_int()
-    apple = @apples[id]
-    ('fruits/show', {}.insert(~apple, apple))
+    id = parameters[~id]
+    if(id == nil)
+      ""
+    else
+      apple = @apples[id.to_int()]
+      ('fruits/show', {}.insert(~apple, apple))
 
   def row(parameters, cookies)
     ('fruits/row', parameters)
--- application_example/public/js/app.js
+++ application_example/public/js/app.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 
 	$('div#apples a').livequery('click',function(){
+		self = $(this)
 		$.ajax({
 			url: $(this).attr('href'),
 			dataType: 'html',
 			complete: function(res, status){
 				if (status == 'success' || status == 'notmodified'){
 					$('#apple').fadeOut(.5, function(){
+						self.parent().children().removeClass('selected')
+						self.addClass('selected')
 						$(this).html(res.responseText).fadeIn()
 					})
 				} else {
@@ -23,6 +26,7 @@ $(document).ready(function() {
 	})
 
 	$('div#menu li').livequery('click',function(){
+		self = $(this)
 		var loaded = false
 		$('#contents').fadeOut(.5, function(){
 			if(!loaded)
@@ -36,6 +40,8 @@ $(document).ready(function() {
 				loaded = true
 				if (status == 'success' || status == 'notmodified'){
 					$('#contents').fadeOut(.5, function(){
+						self.parent().children().removeClass('selected')
+						self.addClass('selected')
 						$(this).html(res.responseText).fadeIn()
 					})
 				} else {
