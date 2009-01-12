3bfcc9850cc8b1395da6f11999999b69ef50279b
Phil Pirj <pirj@mail.ru>
2009-01-12 03:20:25 +0300

    simplifying this a LOT. this is just a sandbox now, handlersl should be used instead

--- application_example/public/js/app.js
+++ application_example/public/js/app.js
@@ -1,93 +1,10 @@
 $(document).ready(function() {
 	
 // fruit store related
+	add_handler('click', '#apples a', '#apple')
+	add_handler('click', '#menu a', '#contents')
 
-	$('div#apples a').livequery('click',function(){
-		self = $(this)
-		$.ajax({
-			url: $(this).attr('href'),
-			dataType: 'html',
-			complete: function(res, status){
-				if (status == 'success' || status == 'notmodified'){
-					$('#apple').fadeOut(.5, function(){
-						self.parent().children().removeClass('selected')
-						self.addClass('selected')
-						$(this).html(res.responseText).fadeIn()
-					})
-				} else {
-					var error = 'Error loading data'
-					$.jGrowl(error, {theme:  'error'})
-					$('#apple').fadeOut(.5, function(){
-						$(this).html(error).fadeIn()
-					})
-				}
-			}
-		})
-
-		return false
-	})
-
-	$('div#menu li').livequery('click',function(){
-		self = $(this)
-		var loaded = false
-		$('#contents').fadeOut(.5, function(){
-			if(!loaded)
-				$(this).html('please wait, loading...').fadeIn()
-		})
-
-		$.ajax({
-			url: $(this).children('a').attr('href'),
-			dataType: 'html',
-			complete: function(res, status){
-				loaded = true
-				if (status == 'success' || status == 'notmodified'){
-					$('#contents').fadeOut(.5, function(){
-						self.parent().children().removeClass('selected')
-						self.addClass('selected')
-						$(this).html(res.responseText).fadeIn()
-					})
-				} else {
-					var error = 'Error loading data'
-					$.jGrowl(error, {theme:  'error'})
-					$('#contents').fadeOut(.5, function(){
-						$(this).html(error).fadeIn()
-					})
-				}
-			}
-		})
-
-		return false
-	})
-	
-	// todo related
-	
-	$('#todo_bar a').livequery('click',function(){
-		var loaded = false
-		$('#todos').fadeOut(.5, function(){
-			if(!loaded)
-				$(this).html('please wait, loading...').fadeIn()
-		})
-
-		$.ajax({
-			url: $(this).attr('href'),
-			dataType: 'html',
-			complete: function(res, status){
-				loaded = true
-				if (status == 'success' || status == 'notmodified'){
-					$('#todos').fadeOut(.5, function(){
-						$(this).html(res.responseText).fadeIn()
-					})
-				} else {
-					var error = 'Error loading data'
-					$.jGrowl(error, {theme:  'error'})
-					$('#todos').fadeOut(.5, function(){
-						$(this).html(error).fadeIn()
-					})
-				}
-			}
-		})
-
-		return false
-	})
+// todo related
+	add_handler('click', '#todo_bar a', '#todos')
 
 })
