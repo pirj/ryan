4f51f721a3c79c62e9499e3e3d40dd3a5676e055
Phil Pirj <pirj@mail.ru>
2009-04-09 10:47:39 +0400

    serving static files too

--- src/core/mochi_shim.erl
+++ src/core/mochi_shim.erl
@@ -6,6 +6,7 @@ out(Req) ->
 
     Abs_Path = Req:get(path),
 	[Full_Path|_] = string:tokens(Abs_Path, [$?]),
+	io:format("fp:~p~n", [Full_Path]),
 	PathParts = string:tokens(Full_Path, [$/]),
 
 	Params = case Method of
@@ -17,10 +18,26 @@ out(Req) ->
 	Cookie = Req:parse_cookie(),
 
 	{Token, Session} = session(Cookie),
+	
+	io:format("~p~n", [Full_Path]),
+	[App|_T] = PathParts,
+	case App of
+		"app" -> serve_dynamic(Req, Abs_Path, Method, PathParts, Token, Params, Session);
+		_ -> serve_static(Req, Full_Path)
+	end.
+	
+serve_dynamic(Req, Abs_Path, Method, PathParts, Token, Params, Session) ->
+	Result = reia_erl:r2e(reia:apply('Ryan', out, [Abs_Path, Method, PathParts, Token, Params])),
+	
+	Response = case Result of
+		{html, Contents} -> {200, [{"Content-Type", "text/html"}] ++ Session, Contents};
+		{status, Status} -> {Status, [Session]};
+		_				-> {}
+	end,
+	Req:respond(Response).
 
-	{html, Result} = reia_erl:r2e(reia:apply('Ryan', out, [Abs_Path, Method, PathParts, Token, Params])),
-
-	Req:respond({200, [{"Content-Type", "text/html"}] ++ Session, Result}).
+serve_static(Req, Full_Path) ->
+	Req:serve_file("", "/users/philpirj/source/ryan/application_example/public" ++ Full_Path).
 
 session(Cookies) ->
 	SID = [X || {"sid", X} <- Cookies],
