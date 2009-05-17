fd0863e658cbd908f1d33709c1393889de368c9d
Phil Pirj <pirj@mail.ru>
2009-05-18 00:32:22 +0400

    some run changes, cli option help, error handling, brave ~128bit session key

--- src/core/ryan_misc.erl
+++ src/core/ryan_misc.erl
@@ -3,11 +3,9 @@
 
 create_session_token() ->
 	random_seed(),
-	RandomOne = integer_to_list(random:uniform(100000000000)),
-	RandomTwo = integer_to_list(random:uniform(100000000000)),
-	MD5One = binary_to_list(erlang:md5(RandomOne)),
-	MD5Two = binary_to_list(erlang:md5(RandomTwo)),
-	base64:encode_to_string(MD5One ++ MD5Two). % ++ Secret !!!!
+	Random = integer_to_list(random:uniform(100000000000000000000000000000000000000)),
+	MD5 = binary_to_list(erlang:md5(Random)),
+	base64:encode_to_string(MD5).
 
 random_seed() -> % Hat tip to Joe Armstrong!
 	{_,_,X} = erlang:now(),
@@ -16,4 +14,3 @@ random_seed() -> % Hat tip to Joe Armstrong!
 	M1 = M * X rem 32767,
 	S1 = S * X rem 32767,
 	put(random_seed, {H1,M1,S1}).
-
